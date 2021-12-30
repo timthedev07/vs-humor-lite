7f0ce0d117bc3dc45dede2cfcfd8eea21fae365b
Tim <harvard90873@gmail.com>
2021-12-30 10:53:30 +0100

    bring in post list

--- webviews/App.svelte
+++ webviews/App.svelte
@@ -1,23 +1,34 @@
 <script>
+  import PostList from "./components/PostList.svelte";
 </script>
 
-<header>
-  <h2>r/ProgrammerHumor</h2>
-  <h5>Brought to you by vs-humor-light</h5>
-</header>
-<main />
+<div id="App">
+  <header>
+    <h2>r/ProgrammerHumor</h2>
+    <h5>Brought to you by vs-humor-lite</h5>
+  </header>
+  <main>
+    <PostList />
+  </main>
+</div>
 
 <style>
   header {
     background-color: rgb(185, 74, 33);
     padding: 10px;
+    width: 100%;
+  }
+
+  main {
+    padding: 8px;
   }
 
   h2,
   h5 {
     text-align: center;
   }
-  main {
+
+  #App {
     min-width: 230px;
   }
 </style>
